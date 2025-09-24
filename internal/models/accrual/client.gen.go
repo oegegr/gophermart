f972df9fd28a53b6ea0ac81970b654784b54607b
Georgy Alexandrov <georgy.alexandrov@kaspersky.com>
2025-09-24 19:11:48 +0300

    fix

--- internal/models/accrual/client.gen.go
+++ internal/models/accrual/client.gen.go
@@ -208,8 +208,8 @@ func (r GetApiOrdersNumberResponse) StatusCode() int {
 }
 
 // GetApiOrdersNumberWithResponse request returning *GetApiOrdersNumberResponse
+//nolint:bodyclose
 func (c *ClientWithResponses) GetApiOrdersNumberWithResponse(ctx context.Context, number string, reqEditors ...RequestEditorFn) (*GetApiOrdersNumberResponse, error) {
-	//nolint:bodyclose
 	rsp, err := c.GetApiOrdersNumber(ctx, number, reqEditors...)
 	if err != nil {
 		return nil, err
