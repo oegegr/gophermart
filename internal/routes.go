0c9f2475b3a610258708e7d8cf365a490f90d4d4
Georgy Alexandrov <georgy.alexandrov@kaspersky.com>
2025-09-24 18:35:15 +0300

    fix

--- internal/routes.go
+++ internal/routes.go
@@ -37,7 +37,7 @@ func NewRouter(
 		r.Get("/api/user/orders", orderHandler.GetUserOrders)
 
 		r.Get("/api/user/balance", balanceHandler.GetUserBalance)
-		r.Post("/api/user/balance/withdrawals", balanceHandler.WithdrawUserBalance)
+		r.Post("/api/user/balance/withdraw", balanceHandler.WithdrawUserBalance)
 		r.Get("/api/user/balance/withdrawals", balanceHandler.GetUserWithdrawals)
 	})
 
